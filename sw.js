0f1810437a7dd9a26e64c07d9b0282699d3955d7
marcellolabr3 <marvinlabre@gmail.com>
2026-07-31 21:24:17 -0300

    visual: polish login screen

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "checkin-cache-v81";
+const CACHE_NAME = "checkin-cache-v82";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
-  "./styles.css?v=20260731a",
+  "./styles.css?v=20260731b",
   "./app.js",
   "./app.js?v=20260330h",
   "./manifest.json",
